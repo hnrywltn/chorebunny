693cf3b37b16f989bdc87f723626421423806bc4
Khiienu <anthonyhuynh123@gmail.com>
2021-08-17 17:40:35 -0500

    Updated about bunny

--- react-app/src/components/Splashpage/Splashpage.js
+++ react-app/src/components/Splashpage/Splashpage.js
@@ -6,6 +6,8 @@ function Splashpage() {
         <div className="wrapper">
             <div className="about-us"> 
                 <img className="about-logo" src="https://imgur.com/qKdCNty.png" alt="about-logo"></img>
+                <p className="about-cb">    This is a clone of TaskRabbit, we have multiple categories and chores that our bunnies can fufill! You can even become one yourself! Sign-up TODAY! 
+                </p>
             </div>
             <div className="feature-chore"> feature chore</div>
             <div className="about-me-links">test</div>
