60938d83f0e8e7efc66f68905c7632fa0d77bcb6
Khiienu <anthonyhuynh123@gmail.com>
2021-08-17 17:21:37 -0500

    added image to about chorebunny

--- react-app/src/components/Splashpage/Splashpage.js
+++ react-app/src/components/Splashpage/Splashpage.js
@@ -1,10 +1,11 @@
 import {Link} from 'react-router-dom';
 import './splashpage.css'
+import './ChoreBunny.png'
 function Splashpage() {
     return(
         <div className="wrapper">
-            <div className="about-us"> about us
-                <div className="image-holder"> test </div>
+            <div className="about-us"> 
+                <img className="about-logo" src="https://imgur.com/qKdCNty.png" alt="about-logo"></img>
             </div>
             <div className="feature-chore"> feature chore</div>
             <div className="about-me-links">test</div>
